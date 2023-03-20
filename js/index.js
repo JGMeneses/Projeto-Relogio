05e287dbf88be6621860d0a5d78da7e2b55512ed
JGMeneses <joaovictorcont15@gmail.com>
2023-03-20 18:05:01 -0300

    Projeto-Relógio

---
+++ js/index.js
@@ -0,0 +1,24 @@
+
+const currentTime = () => {
+  const Hora = document.querySelector(".Hora");
+  const markingSeconds = document.querySelector(".marking-seconds");
+
+  let date = new Date();
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
+
+  hh = hh < 10 ? `0${hh}` : hh;
+  mm = mm < 10 ? `0${mm}` : mm;
+  ss = ss < 10 ? `0${ss}` : ss;
+
+  const ssDegrees = (ss / 60) * 360;
+  markingSeconds.style.transform = `rotate(${ssDegrees}deg)`;
+
+  let time = `${hh}:${mm}`;
+  Hora.innerText = time;
+};
+
+currentTime();
+setInterval(currentTime, 1000);
+
